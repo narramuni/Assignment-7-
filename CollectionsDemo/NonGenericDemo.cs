a3c4bd275db9daaca33bd5816005f0cc49604beb
narramuni <nmspavan@gmail.com>
2023-10-11 20:12:19 +0530

    Add project files.

---
+++ CollectionsDemo/NonGenericDemo.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    internal class NonGenericDemo
+    {
+        static void Main(string[] args)
+        {
+
+            Hashtable ht = new Hashtable();
+
+            ht.Add(1, "Hello");
+            ht.Add(2, 246);
+            ht.Add("jhon", 3.246f);
+            ht.Add(226, 12);
+
+            Console.WriteLine("Hashtable 1:");
+
+
+            foreach (DictionaryEntry item in ht) {
+
+                Console.WriteLine(item.Key + " " + item.Value);    
+            
+            }
+
+            Console.WriteLine("@@@@@@");
+
+            ht.Remove(226);
+            Console.WriteLine(  "after removing ");
+            foreach (DictionaryEntry item in ht)
+            {
+
+                Console.WriteLine(item.Key + " " + item.Value);
+
+            }
+
+            Hashtable htt = new Hashtable(ht);
+
+            Console.WriteLine("\n new Hashtable (htt):");
+
+            foreach (DictionaryEntry item in ht)
+            {
+
+                Console.WriteLine(item.Key + " " + item.Value);
+
+            }
+
+            Console.WriteLine("ht");
+
+
+            foreach (DictionaryEntry item in ht)
+            {
+
+                Console.WriteLine(item.Key + " " + item.Value);
+
+            }
+
+
+
+
+
+            //Queue queue = new Queue();
+            //queue.Enqueue("India");
+            //queue.Enqueue("Aus");
+            //queue.Enqueue("Eng");
+
+            //foreach (var item in queue) {
+            //    Console.WriteLine(  item);
+
+            //}
+
+
+            //queue.Dequeue();
+            //Console.WriteLine("After Dequeue");
+            //foreach(var item  in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Queue q = new Queue(queue);
+            //Console.WriteLine("queue");
+
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("q:");
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //queue.Clear();
+            //Console.WriteLine("Original Queue after clearing:");
+
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+
+            Console.ReadLine();
+
+
+            //Stack s = new Stack();
+            //s.Push(76);
+
+
+            //s.Push("Hello");
+
+
+            //s.Push(96.264d);
+
+            //foreach (var item in s)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("**********");
+
+
+            //s.Pop();
+            //Console.WriteLine("Stack after one pop operation:");
+
+            //foreach (var item in s)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("**********");         
+            //Stack stack = new Stack(s);
+            //Console.WriteLine("s:");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("stack:");
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("**********");
+
+
+            //s.Clear();
+            //Console.WriteLine("Original Stack after clearing:");
+            //foreach (var item in s)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("**********");
+            //Console.ReadLine();
+        }
+
+    }
+}
+
+
---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList a = new ArrayList();
+            a.Add(24);
+            a.Add(4.6843);
+            a.Add(25);
+            a.Add("giri");
+            a.Add("one");
+            a.Add(5612);
+            a.Add("two");
+            a.Add(12);
+            a.Add(96);
+            a.Add(92);
+            Console.WriteLine(  );
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+
+            }
+            a.Remove("25");
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            object[] objarr = new object[a.Count];
+            a.CopyTo(objarr);
+            int i = 0;
+
+                
+
+            foreach (object obj in objarr)
+            {
+                Type t = obj.GetType(); 
+
+                if (t != typeof(int))
+                {
+                    objarr[i] = 0; 
+                }
+                i++;
+            }
+
+            Console.WriteLine("Now the contents of the array:");
+
+            foreach (var item in objarr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Sort(objarr);
+
+            Array.Reverse(objarr);
+
+            Console.WriteLine("Sorted and Reversed Array:");
+
+            foreach (var item in objarr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
